aeb9e92d606efcf3a0e6e2d8fdae2457b007e062
Tina Huynh <tinamchuynh@gmail.com>
2025-04-25 02:09:59 -0700

    Refactors screen size detection hook for flexibility
    Enhances the custom hook to allow configurable
    debounce delay and screen width threshold.
    
    Updates the logic to simplify the condition for
    determining larger screen sizes, improving usability
    and responsiveness.
    
    Removes hardcoded values for better adaptability
    to different screen size requirements.

--- src/lib/screens/useLargerScreen.ts
+++ src/lib/screens/useLargerScreen.ts
@@ -1,29 +1,43 @@
 import { useState, useEffect } from "react";
 import { debounce } from "./debounce";
 
+
 /**
- * Custom hook to determine if the screen size is larger (between 1025px and 1440px).
+ * A custom React hook that determines if the screen width exceeds a certain threshold.
+ * It listens to the window resize event and updates the state accordingly, with an optional debounce delay.
+ *
+ * @param {number} debounceDelay - The delay in milliseconds for debouncing the resize event. Default is 300ms.
+ * @returns {boolean} - A boolean indicating whether the screen width is larger than the threshold.
+ *
+ * Usage:
+    const debouncedHandleResize = useMemo(
+      () => debounce(handleResize, debounceDelay),
+      [debounce, debounceDelay]
+  const [isLargerScreen, setIsLargerScreen] = useState<boolean>(() => window.innerWidth > 1024);
  */
-const useLargerScreen = () => {
-  const [isLargerScreen, setIsLargerScreen] = useState(false);
+const useIsLargerScreen = (
+  debounceDelay: number = 300,
+  threshold: number = 1024
+) => {
+  const [isLargerScreen, setIsLargerScreen] = useState<boolean>(false);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsLargerScreen(window.innerWidth > 1024 && window.innerWidth <= 1440);
+      setIsLargerScreen(window.innerWidth > threshold);
     };
 
-    const debouncedHandleResize = debounce(handleResize, 300);
+    const debouncedHandleResize = debounce(handleResize, debounceDelay);
 
     window.addEventListener("resize", debouncedHandleResize);
 
     handleResize();
 
     return () => {
-      window.removeEventListener("resize", debouncedHandleResize);
-    };
-  }, []);
+    window.removeEventListener("resize", debouncedHandleResize);
+  };
+  }, [debounceDelay, threshold]);
 
   return isLargerScreen;
 };
 
-export default useLargerScreen;
+export default useIsLargerScreen;
