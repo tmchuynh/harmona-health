15a18c9d18983b6ec5970ea4bc91b7de2bc8f6b4
Tina Huynh <tinamchuynh@gmail.com>
2025-04-24 23:15:20 -0700

    Adds navigation menu configuration and new toggle component
    Introduces a structured navigation menu with main and secondary pages to enhance user experience.
    
    Adds a new toggle component from Radix UI for better UI control.
    
    Updates package dependencies to include the new toggle component.
    
    Improves overall code organization and maintainability.

---
+++ src/lib/constants/navigation/menu.ts
@@ -0,0 +1,92 @@
+import { NavigationMenu } from "@/lib/interfaces&types/types";
+import { SquaresPlusIcon } from "@heroicons/react/24/outline";
+import { ChartPieIcon, PhoneIcon, PlayCircleIcon } from "lucide-react";
+
+export const mainPages: NavigationMenu[] = [
+  {
+    name: "Programs",
+    description: "Get a better understanding of your traffic",
+    href: "/programs",
+    icon: ChartPieIcon,
+  },
+  {
+    name: "Sucess Stories",
+    description: "Connect with third-party tools",
+    href: "/programs/success-stories",
+    icon: SquaresPlusIcon,
+  },
+  {
+    name: "FAQs",
+    description: "Connect with third-party tools",
+    href: "/programs/FAQ",
+    icon: SquaresPlusIcon,
+  },
+  {
+    name: "Digital Products",
+    description: "Connect with third-party tools",
+    href: "/wellness-library/digital-products",
+    icon: SquaresPlusIcon,
+  },
+];
+
+export const seccondaryPages: NavigationMenu[] = [
+  {
+    name: "Wellness Library",
+    description: "Get a better understanding of your traffic",
+    href: "/wellness-library",
+    icon: ChartPieIcon,
+  },
+  {
+    name: "Wellness Articles",
+    description: "Get a better understanding of your traffic",
+    href: "/wellness-library/articles",
+    icon: ChartPieIcon,
+  },
+  {
+    name: "Health Toolkit",
+    description: "Connect with third-party tools",
+    href: "/wellness-library/digital-products/health-toolkit",
+    icon: SquaresPlusIcon,
+  },
+];
+
+export const callsToAction: NavigationMenu[] = [
+  {
+    name: "Affiliates & Partnerships",
+    href: "/about/affiliates-and-partnerships",
+    icon: PlayCircleIcon,
+  },
+  {
+    name: "Assessments",
+    href: "/wellness-library/digital-products/assessments",
+    icon: PhoneIcon,
+  },
+];
+
+export const callsToActionSecondary: NavigationMenu[] = [
+  {
+    name: "Accessibility Statement",
+    href: "/about/accessibillity-statement",
+    icon: PlayCircleIcon,
+  },
+  {
+    name: "Wellness Dictionary",
+    href: "/wellness-library/digital-products/wellness-dictionary",
+    icon: PhoneIcon,
+  },
+];
+
+export const singlePage: NavigationMenu[] = [
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Events and Workshops",
+    href: "/wellness-library/events-and-workshops",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
--- src/lib/interfaces&types/types.ts
+++ src/lib/interfaces&types/types.ts
@@ -1,4 +1,33 @@
+import { IconType } from "react-icons/lib";
+
 export type NotFoundContextType = {
   isNotFound: boolean;
   setNotFound: (value: boolean) => void;
+};
+
+export type DynamicButtonProps = {
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "accent"
+    | "tertiary"
+    | "fancy"
+    | "destructive"
+    | "ghost"
+    | "link"
+    | "disabled";
+  text: string;
+  icon?: IconType;
+  onClick?: () => void;
+  className?: string;
+  iconClassName?: string;
+};
+
+export type NavigationMenu = {
+  name: string;
+  description?: string;
+  href: string;
+  icon?: IconType;
 };
