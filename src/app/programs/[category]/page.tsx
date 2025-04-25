c45bdecded345bdd4eca78f6b531c30c169f8905
Tina Huynh <tinamchuynh@gmail.com>
2025-04-25 01:34:55 -0700

    Adds service filtering and display based on category
    Implements filtering of sub-services by category ID
    from the URL, enhancing user navigation.
    
    Introduces a grid layout to display services with
    titles, introductions, and descriptions using cards.
    
    Improves overall user experience in the services page.

--- src/app/programs/[category]/page.tsx
+++ src/app/programs/[category]/page.tsx
@@ -1,17 +1,49 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { serviceCategories } from "@/lib/constants/services/serviceCategory";
+import { subServices } from "@/lib/constants/services/services";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 export default function Page() {
+  const segments = usePathname().split("/");
+  const categoryId = segments[segments.length - 1];
+  const filteredServices = subServices.filter((service) =>
+    serviceCategories.some(() => categoryId === service.categoryId)
+  );
+
   return (
     <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
-      <h5>Blank blank blank blank</h5>
       <h1>Blank</h1>
+      <h5>Blank blank blank blank</h5>
 
-      <p>
+      <p className="pb-8">
         Ipsum excepteur dolore id velit adipisicing magna quis in commodo sint
         sit nostrud dolor. Eiusmod amet adipisicing consequat ea. Aute voluptate
         sunt sint elit qui aute ea non eiusmod labore. Ea voluptate ex ullamco
         qui aliqua qui minim voluptate ut incididunt nostrud. Cillum elit minim
         cupidatat officia.
       </p>
+
+      <div className="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {filteredServices.map((service, index) => {
+          return (
+            <Link
+              key={`${service.id}-${index}`}
+              href={`/programs/${categoryId}/${service.id}`}
+            >
+              <Card>
+                <CardContent>
+                  <h2>{service.title}</h2>
+                  <p>{service.introduction}</p>
+                  <p>{service.description}</p>
+                </CardContent>
+              </Card>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
