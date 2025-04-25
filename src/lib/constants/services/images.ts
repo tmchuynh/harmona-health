d1745d57851b6b68b15fdd598c263efb20241660
Tina Huynh <tinamchuynh@gmail.com>
2025-04-25 02:10:12 -0700

    Removes unused image link from constants
    Eliminates an outdated image link from the data array
    to streamline the codebase and maintain relevance.
    
    No additional context provided.

--- src/lib/constants/services/images.ts
+++ src/lib/constants/services/images.ts
@@ -15,10 +15,6 @@ export const data = [
     imageLink:
       "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
   },
-  {
-    imageLink:
-      "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
-  },
 ];
 
 export const tabData = [
