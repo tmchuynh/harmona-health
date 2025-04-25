8381133109bfa3a0deba87bfdb8f1600222206d0
Tina Huynh <tinamchuynh@gmail.com>
2025-04-25 02:44:26 -0700

    Replaces Link with Card component and adds dynamic button
    Enhances the user interface by replacing Link with a Card component for better styling and layout.
    
    Implements a DynamicButton for category exploration, providing a more interactive experience.
    
    Utilizes the useRouter hook for navigation, improving routing efficiency.

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,8 +1,11 @@
-import { Card, CardContent } from "@/components/ui/card";
+"use client";
+
+import DynamicButton from "@/components/button/button-dynamic";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { serviceCategories } from "@/lib/constants/services/serviceCategory";
-import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
 export default function Page() {
+  const router = useRouter();
   return (
     <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
       <h1>Blank</h1>
@@ -10,18 +13,22 @@ export default function Page() {
 
       <div className="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {serviceCategories.map((category, index) => (
-          <Link
+          <Card
             key={`${category.id}-${index}`}
-            href={`/programs/${category.id}`}
+            className="group flex flex-col justify-between h-full min-h-[20rem] cursor-pointer"
+            onClick={() => router.push(`/programs/${category.id}`)}
           >
-            <Card>
-              <CardContent>
-                <category.icon className="mb-5 w-10 h-10" />
-                <h2>{category.title}</h2>
-                <p>{category.description}</p>
-              </CardContent>
-            </Card>
-          </Link>
+            <CardContent>
+              <category.icon className="mb-5 w-10 h-10 group-hover:text-tertiary" />
+              <h2 className="underline-offset-4 group-hover:underline decoration-1">
+                {category.title}
+              </h2>
+              <p>{category.description}</p>
+            </CardContent>
+            <CardFooter>
+              <DynamicButton>Explore This Category</DynamicButton>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
