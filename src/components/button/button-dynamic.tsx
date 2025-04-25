1dee9b640964e0ff5c6a504911e645f73282b67b
Tina Huynh <tinamchuynh@gmail.com>
2025-04-24 22:23:21 -0700

    Adds dynamic button components and theme toggler
    Implements "Back to Top" and "Theme Toggle" buttons for improved user navigation and accessibility.
    
    Introduces a reusable dynamic button that supports icons and varied styles, enhancing UI flexibility.
    
    Fixes #456

---
+++ src/components/button/button-dynamic.tsx
@@ -0,0 +1,66 @@
+import { cn } from "@/lib/utils";
+import React, { JSX } from "react";
+import { Button } from "../ui/button";
+import { DynamicButtonProps } from "@/lib/interfaces&types/types";
+import { FaLeaf } from "react-icons/fa";
+
+/**
+ * A dynamic button component that can display icons alongside its content.
+ *
+ * @param variant - The visual style of the button ("default" by default).
+ * @param children - The content to display inside the button.
+ * @param Icon - The icon component to display (FaLeaf by default).
+ * @param onClick - The function to call when the button is clicked.
+ * @param className - Additional CSS classes to apply to the button.
+ * @param iconClassName - Additional CSS classes to apply to the icon container.
+ *
+ * @returns JSX Element representing the button.
+ *
+ * @example
+ * ```tsx
+ * <DynamicButton
+ *   variant="primary"
+ *   onClick={() => console.log("Button clicked")}
+ *   className="my-custom-class"
+ *   icon={FaHeart}
+ * >
+ *   Click Me
+ * </DynamicButton>
+ * ```
+ */
+export default function DynamicButton({
+  variant = "default",
+  children,
+  icon: Icon = FaLeaf,
+  onClick,
+  className,
+  iconClassName,
+}: Omit<DynamicButtonProps, "text"> & {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <Button
+      variant={variant}
+      onClick={onClick}
+      className={cn(
+        "mt-2 group gap-0 flex w-full mx-auto font-[Cormorant] uppercase",
+        {
+          "inline w-fit p-0 m-0": variant === "link",
+        },
+        className
+      )}
+    >
+      {Icon && (
+        <span className={cn("inline-block px-4", iconClassName)}>
+          <Icon className="w-4 h-4" />
+        </span>
+      )}
+      <span className="px-3">{children}</span>
+      {Icon && variant !== "link" && (
+        <span className={cn("inline-block px-4", iconClassName)}>
+          <Icon className="w-4 h-4" />
+        </span>
+      )}
+    </Button>
+  );
+}
