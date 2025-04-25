da67bd07240b095d527b2d041847e675b07d7fdc
Tina Huynh <tinamchuynh@gmail.com>
2025-04-24 23:53:19 -0700

    Renames program files and organizes category structure
    Moves program files to a new category structure for better organization.
    
    Introduces a new page component for the category with a layout and placeholders for dynamic content.
    
    Updates the main program page to display service categories using cards for a cleaner UI.
    
    Improves code maintainability and user experience.

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,17 +1,23 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { serviceCategories } from "@/lib/constants/services/serviceCategory";
 import React from "react";
 export default function Page() {
   return (
     <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
-      <h5>Blank blank blank blank</h5>
       <h1>Blank</h1>
+      <h5>Blank blank blank blank</h5>
 
-      <p>
-        Ipsum excepteur dolore id velit adipisicing magna quis in commodo sint
-        sit nostrud dolor. Eiusmod amet adipisicing consequat ea. Aute voluptate
-        sunt sint elit qui aute ea non eiusmod labore. Ea voluptate ex ullamco
-        qui aliqua qui minim voluptate ut incididunt nostrud. Cillum elit minim
-        cupidatat officia.
-      </p>
+      <div className="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {serviceCategories.map((category, index) => (
+          <Card key={`${category.id}-${index}`}>
+            <CardContent>
+              <category.icon className="mb-5 w-10 h-10" />
+              <h2>{category.title}</h2>
+              <p>{category.description}</p>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
