ff1e2a81f7518c08f1353b02f434e1e96c84b7a3
Tina Huynh <tinamchuynh@gmail.com>
2025-04-25 01:34:27 -0700

    Implements service detail page with tier pricing
    Enhances service page to display detailed information
    about services, including descriptions, key points,
    program outcomes, and goals.
    
    Introduces a table for blackout days with visual indicators
    for availability, improving user experience in service selection.

--- src/app/programs/[category]/[program]/page.tsx
+++ src/app/programs/[category]/[program]/page.tsx
@@ -1,17 +1,150 @@
-import React from "react";
-export default function Page() {
-  return (
-    <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
-      <h5>Blank blank blank blank</h5>
-      <h1>Blank</h1>
+"use client";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { IoCloseSharp } from "react-icons/io5";
 
-      <p>
-        Ipsum excepteur dolore id velit adipisicing magna quis in commodo sint
-        sit nostrud dolor. Eiusmod amet adipisicing consequat ea. Aute voluptate
-        sunt sint elit qui aute ea non eiusmod labore. Ea voluptate ex ullamco
-        qui aliqua qui minim voluptate ut incididunt nostrud. Cillum elit minim
-        cupidatat officia.
-      </p>
-    </div>
+import { subServiceInformation } from "@/lib/constants/services/serviceInformation";
+import { subServices } from "@/lib/constants/services/services";
+import { serviceTierInformation } from "@/lib/constants/services/tierInformation";
+import { cn } from "@/lib/utils";
+import { capitalize, formatCurrency } from "@/lib/utils/format";
+import { usePathname } from "next/navigation";
+import { FaCheckDouble, FaXRay } from "react-icons/fa";
+export default function Page() {
+  const segments = usePathname().split("/");
+  const serviceId = segments[segments.length - 1];
+
+  const information = subServiceInformation.find(
+    (info) => info.subServiceId === serviceId
+  );
+
+  const service = subServices.find((service) => service.id === serviceId);
+
+  const filteredTiers = serviceTierInformation.filter(
+    (tier) => serviceId === tier.subServiceId
+  );
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return (
+    <>
+      {service && information ? (
+        <article className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
+          <h1>Blank</h1>
+          <h5>Blank blank blank blank</h5>
+
+          <p className="pb-8">
+            Ipsum excepteur dolore id velit adipisicing magna quis in commodo
+            sint sit nostrud dolor. Eiusmod amet adipisicing consequat ea. Aute
+            voluptate sunt sint elit qui aute ea non eiusmod labore. Ea
+            voluptate ex ullamco qui aliqua qui minim voluptate ut incididunt
+            nostrud. Cillum elit minim cupidatat officia.
+          </p>
+          <div className="gap-4 grid grid-cols-1 md:grid-cols-2 pb-3">
+            <div>
+              <h2>{service.title}</h2>
+              <p>{information.introduction}</p>
+              <p>{service.description}</p>
+            </div>
+            <section className="gap-5 grid grid-cols-1 lg:grid-cols-2">
+              <div>
+                <h3>Key Points</h3>
+                <ul>
+                  {information.keypoints.map((keypoint, index) => (
+                    <li key={`${keypoint}-${index}`}>{keypoint}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h3>Program Outcomes</h3>
+                <ul>
+                  {information.outcomes.map((outcome, index) => (
+                    <li key={`${outcome}-${index}`}>{outcome}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h3>Program Goals</h3>
+                <ul>
+                  {information.goals.map((goal, index) => (
+                    <li key={`${goal}-${index}`}>{goal}</li>
+                  ))}
+                </ul>
+              </div>
+            </section>
+          </div>
+
+          <section>
+            {filteredTiers.map((tier, index) => (
+              <div
+                key={`${tier.tier}-${index}`}
+                className={cn("flex flex-col gap-2 py-5 xl:pr-5  xl:w-1/2", {
+                  "border-b-2": index !== filteredTiers.length - 1,
+                })}
+              >
+                <div className="flex justify-between gap-4">
+                  <h2>{capitalize(tier.tier)} Tier</h2>
+                  <p>{formatCurrency(tier.price)}</p>
+                </div>
+                <p>{service.description}</p>
+                {tier.blackOutDays && (
+                  <>
+                    <h5 className="mb-3 underline underline-offset-2">
+                      Black Out Dates
+                    </h5>
+                    <Table>
+                      <TableHeader>
+                        <TableRow className="flex justify-around text-center">
+                          <TableHead>S</TableHead>
+                          <TableHead>M</TableHead>
+                          <TableHead>T</TableHead>
+                          <TableHead>W</TableHead>
+                          <TableHead>TH</TableHead>
+                          <TableHead>F</TableHead>
+                          <TableHead>S</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        <TableRow className="flex justify-around text-center">
+                          {days.map((dayName, dayIndex) => (
+                            <TableCell key={dayIndex}>
+                              {tier.blackOutDays?.includes(dayName) ? (
+                                <IoCloseSharp
+                                  size={20}
+                                  className="text-red-500"
+                                />
+                              ) : (
+                                <FaCheckDouble
+                                  className="text-green-500"
+                                  size={20}
+                                />
+                              )}
+                            </TableCell>
+                          ))}
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </>
+                )}
+              </div>
+            ))}
+          </section>
+        </article>
+      ) : null}
+    </>
   );
 }
