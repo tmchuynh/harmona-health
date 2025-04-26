dd29d5932a631e709b14057a42b3f5de2b3343dc
Tina Huynh <tinamchuynh@gmail.com>
2025-04-26 13:02:15 -0700

    Refactors imageHosts in next.config.ts to remove unused domains and streamline configuration

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,9 @@
 import type { NextConfig } from "next";
 
 const imageHosts = [
-  "www.psychiatry.org",
-  "images-na.ssl-images-amazon.com",
-  "images.wiley.com",
-  "oc87recoverydiaries.org",
-  "cdn.themighty.com",
-  "www.madinamerica.com",
-  "www.meditationoasis.com",
-  "www.mind.org.uk",
-  "images-na.ssl-images-amazon.com",
-  "www.mindful.org",
-  "tailwindcss.com",
-  "www.calmsage.com",
-  "www.mindfuleating.org",
-  "www.headspace.com",
-  "upload.wikimedia.org",
-  "biglifejournal.com",
-  "minddoc.de",
-  "www.sanvello.com",
-  "cdn.youper.ai",
-  "www.meditationapp.com",
-  "www.simplehabit.com",
-  "www.smilingmind.com.au",
-  "www.wakingup.com",
-  "www.breathe.com",
+  "images.unsplash.com",
   "www.research.colostate.edu",
+  "tailwindcss.com",
 ];
 
 const nextConfig: NextConfig = {
