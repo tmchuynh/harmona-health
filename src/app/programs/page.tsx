8a7ec93f0bd1b71a4ff63f899e22c48b69cf6cf1
Tina Huynh <tinamchuynh@gmail.com>
2025-04-26 13:02:29 -0700

    Refactors Page component to update headings and add descriptive content for health programs

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -4,10 +4,23 @@ import { serviceCategories } from "@/lib/constants/services/serviceCategory";
 export default function Page() {
   return (
     <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
-      <h1>Blank</h1>
-      <h5>Blank blank blank blank</h5>
+      <h1>Health Programs</h1>
+      <h5>Explore our complete wellness protocols tailored to your goals</h5>
 
-      <div className="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <p>
+        At Harmona Health, we offer a diverse range of services designed to
+        promote holistic well-being and empower individuals and organizations to
+        thrive. From Wellness Programs that harmonize mind, body, and spirit, to
+        Nutrition Coaching that provides personalized dietary guidance, our
+        services cater to every aspect of health. Whether you're looking to
+        enhance physical fitness through Fitness Training, seek emotional
+        balance with Mental Health Support, or foster a healthier workplace with
+        Corporate Wellness solutions, our expert-led programs are tailored to
+        meet your unique needs and goals. Explore our offerings and take the
+        first step toward a healthier, more fulfilling life.
+      </p>
+
+      <div className="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-2 md:my-4 lg:my-8 xl:my-12">
         {serviceCategories.map((category, index) => (
           <ProgramCategoryCard
             category={category}
