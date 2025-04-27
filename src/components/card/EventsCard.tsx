754059a384b93f121782fe84556dc42a942a9e3d
Tina Huynh <tinamchuynh@gmail.com>
2025-04-27 15:17:57 -0700

    Updates event card to display category instead of title
    Replaces event title with category in the event card for better context.
    
    Enhances styling for improved hover effects on event titles.
    
    No functional changes to event descriptions or links.

--- src/components/card/EventsCard.tsx
+++ src/components/card/EventsCard.tsx
@@ -4,17 +4,17 @@ export default function EventsCard({ event }: { event: Events }) {
   return (
     <article
       key={event.id}
-      className="flex flex-col justify-between items-start max-w-xl"
+      className="group flex flex-col items-start max-w-xl"
     >
       <div className="flex items-center gap-x-4 text-xs">
         <time dateTime={event.date}>{event.date}</time>
-        <a href={event.href}>{event.title}</a>
+        <a href={event.href}>{event.category}</a>
       </div>
-      <div className="group relative">
-        <h3>
+      <div className="relative">
+        <h3 className="underline-offset-4 group-hover:underline decoration-1">
           <a href={event.href}>{event.title}</a>
         </h3>
-        <p className="mt-5">{event.description}</p>
+        <p>{event.description}</p>
       </div>
     </article>
   );
