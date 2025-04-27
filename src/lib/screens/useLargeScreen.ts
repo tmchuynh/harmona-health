db2558ba74bc75cfd90641728c5b710300ac9c78
Tina Huynh <tinamchuynh@gmail.com>
2025-04-27 13:21:27 -0700

    Add custom hook useLargeScreen to determine screen width for responsive design

---
+++ src/lib/screens/useLargeScreen.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { debounce } from "./debounce";
+
+/**
+ * Custom hook that determines if the current screen width is considered "medium".
+ *
+ * This hook sets up an event listener for the window resize event and updates
+ * the state to indicate whether the screen width is less than or equal to 1024 pixels.
+ *
+ * @returns {boolean} - A boolean value indicating if the screen width is medium (<= 1024 pixels).
+ *
+ * @example
+ * const isLargeScreen = useLargeScreen();
+ *
+ * if (isLargeScreen) {
+ *   // Do something for medium screens
+ * }
+ */
+const useLargeScreen = (): boolean => {
+  const [isLargeScreen, setIsLargeScreen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth <= 1024);
+    };
+
+    const debouncedHandleResize = debounce(handleResize, 200);
+
+    window.addEventListener("resize", debouncedHandleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  }, []);
+
+  return isLargeScreen;
+};
+
+export default useLargeScreen;
