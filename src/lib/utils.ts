0beb13f7602218e7000ecc927ab4aad8f981af5b
Tina Huynh <tinamchuynh@gmail.com>
2025-04-27 13:54:40 -0700

    Removes unused import from utility functions
    Cleans up the code by eliminating the unused toolsMap import.
    
    This change enhances code clarity and maintainability.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { toolsMap } from "./resources/toolkits/tools";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
