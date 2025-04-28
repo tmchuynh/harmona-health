25cfd626816deacffbdad26d40665b4c8d9e0c79
Tina Huynh <tinamchuynh@gmail.com>
2025-04-28 00:56:14 -0700

    Refactors layout components to enhance titles display
    Updates title rendering in layout components to include toolkit names for better context.
    
    Removes unnecessary console logs and adds a new utility function to slice off the last word from the toolkit name.
    
    Improves code organization by consolidating imports.
    
    No related issues.

--- src/app/wellness-library/digital-resources/mental-health-toolkit/(group)/behavioral-activation-activities/[tools]/page.tsx
+++ src/app/wellness-library/digital-resources/mental-health-toolkit/(group)/behavioral-activation-activities/[tools]/page.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+"use client";
+
+import { useToolContext } from "@/context/toolContext";
+import { getToolResource } from "@/lib/utils";
+import { formatUrlToID } from "@/lib/utils/format";
+import React, { useEffect, useState } from "react";
 export default function Page() {
+  const { tool, toolKit, toolKitID, toolInformation } = useToolContext();
+  const [loading, setLoading] = useState(true);
+  const [toolData, setToolData] = useState<[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Await the Promise from getToolResource
+        const data = await getToolResource(toolKit, tool, formatUrlToID(tool));
+        setToolData(data);
+      } catch (error) {
+        console.error("Failed to load affirmation cards:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [toolKit, toolKitID]);
+
+  console.log("toolData", toolData);
+
   return (
     <div className="mx-auto pt-3 md:pt-5 lg:pt-9">
       <h1>Harmona Health's Affiliates and Partnerships</h1>
