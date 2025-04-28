9a14c34c62bfa2afc0b40c4c8ea04bc5ba9ff566
Tina Huynh <tinamchuynh@gmail.com>
2025-04-27 19:14:00 -0700

    Adds ToolProvider for enhanced context management
    Implements ToolProvider to wrap layout component,
    allowing for improved context management of tool-related data.
    
    Ensures safe access to corresponding tool information
    by implementing optional chaining.
    
    Improves code structure for better readability and maintainability.

--- src/app/wellness-library/digital-resources/mental-health-toolkit/(group)/affirmation-cards/[tools]/layout.tsx
+++ src/app/wellness-library/digital-resources/mental-health-toolkit/(group)/affirmation-cards/[tools]/layout.tsx
@@ -1,9 +1,12 @@
 "use client";
+
+import { ToolProvider } from "@/context/toolContext";
 import { toolsMap } from "@/lib/resources/toolkits/tools";
 import { capitalize, formatUrlToID } from "@/lib/utils/format";
 import { usePathname } from "next/navigation";
-import { ReactNode } from "react";
-export default function Page({ children }: { children: ReactNode }) {
+import React, { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
   const tool = usePathname().split("/").slice(-1)[0];
   const toolKit = usePathname().split("/").slice(-2)[0];
 
@@ -12,16 +15,17 @@ export default function Page({ children }: { children: ReactNode }) {
 
   const correspondingTools = toolsMap[toolKitID as keyof typeof toolsMap];
 
-  const toolInformation = correspondingTools.find(
+  const toolInformation = correspondingTools?.find(
     (toolInfo) => toolInfo.title === toolName
   );
 
   return (
-    <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-10/12 md:w-11/12">
-      <h1>{capitalize(tool)}</h1>
-      <h5>{toolInformation?.description}</h5>
-
-      {children}
-    </div>
+    <ToolProvider value={{ tool, toolKit, toolKitID, toolInformation }}>
+      <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-10/12 md:w-11/12">
+        <h1>{capitalize(tool)}</h1>
+        <h5>{toolInformation?.description}</h5>
+        {children}
+      </div>
+    </ToolProvider>
   );
 }
