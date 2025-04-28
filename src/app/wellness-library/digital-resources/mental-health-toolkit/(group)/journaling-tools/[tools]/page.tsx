ca27b67a7082625045bfe9549e0eb58831e04bbe
Tina Huynh <tinamchuynh@gmail.com>
2025-04-27 22:40:25 -0700

    Implement data fetching for journaling tools and enhance loading state management

--- src/app/wellness-library/digital-resources/mental-health-toolkit/(group)/journaling-tools/[tools]/page.tsx
+++ src/app/wellness-library/digital-resources/mental-health-toolkit/(group)/journaling-tools/[tools]/page.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
+import { useToolContext } from "@/context/toolContext";
+import { getToolResource } from "@/lib/utils";
+import { formatUrlToID } from "@/lib/utils/format";
+import React, { useEffect, useState } from "react";
 export default function Page() {
+  const { tool, toolKit, toolKitID, toolInformation } = useToolContext();
+  const [journal, setJournal] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Await the Promise from getToolResource
+        const data = await getToolResource(toolKit, tool, formatUrlToID(tool));
+        setJournal(data);
+      } catch (error) {
+        console.error("Failed to load affirmation cards:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [toolKit, toolKitID]);
+
+  console.log("journal", journal);
+  // console.log("toolInformation", toolInformation);
+  console.log("tool page", formatUrlToID(tool));
+
+  if (!toolInformation) {
+    return <div>Error: Tool information not found.</div>;
+  }
+
+  if (loading) {
+    return <div>Loading affirmation cards...</div>;
+  }
   return (
-    <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-10/12 md:w-11/12">
+    <div className="mx-auto pt-3 md:pt-5 lg:pt-9">
       <h1>Harmona Health's Affiliates and Partnerships</h1>
       <h5>Blank blank blank blank</h5>
 
