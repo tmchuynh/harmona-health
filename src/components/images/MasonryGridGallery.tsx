674d13be830188310dc95530a76ab4f5b3a54fc8
Tina Huynh <tinamchuynh@gmail.com>
2025-04-28 00:55:58 -0700

    fix: Update image source URLs in MasonryGridGallery for consistency

--- src/components/images/MasonryGridGallery.tsx
+++ src/components/images/MasonryGridGallery.tsx
@@ -54,7 +54,7 @@ export function MasonryGridGallery() {
         <div>
           <Image
             className="rounded-lg max-w-full h-auto object-cover object-center"
-            src="https://docs.material-tailwind.com/img/team-3.jpg"
+            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
             alt="gallery-photo"
             width={500}
             height={500}
@@ -74,7 +74,7 @@ export function MasonryGridGallery() {
         <div>
           <Image
             className="rounded-lg max-w-full h-auto object-cover object-center"
-            src="https://docs.material-tailwind.com/img/team-3.jpg"
+            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
             alt="gallery-photo"
             width={500}
             height={500}
