a090ffe0d33420e42c7054a529564ef35a36fe6b
Tina Huynh <tinamchuynh@gmail.com>
2025-04-29 02:05:31 -0700

    Refactor BehavioralActivitiesCard to use 'data' prop for consistency in data access

--- src/components/card/BehavioralActivitiesCard.tsx
+++ src/components/card/BehavioralActivitiesCard.tsx
@@ -1,24 +1,20 @@
 import { BehavioralActivities } from "@/lib/interfaces&types/resources";
 import React from "react";
 
-const BehavioralActivitiesCard = ({
-  activity,
-}: {
-  activity: BehavioralActivities;
-}) => {
+const BehavioralActivitiesCard = ({ data }: { data: BehavioralActivities }) => {
   return (
     <div className="bg-card shadow-md mx-auto p-9 rounded-lg">
-      <h1 className="mb-4 font-bold text-2xl">{activity.title}</h1>
+      <h1 className="mb-4 font-bold text-2xl">{data.title}</h1>
 
       <section className="mb-6">
-        <p>{activity.introduction}</p>
-        <p>{activity.description}</p>
+        <p>{data.introduction}</p>
+        <p>{data.description}</p>
       </section>
 
       <section className="mb-6">
         <h3>Steps</h3>
         <ol className="pl-6 list-decimal">
-          {activity.steps.map((step, index) => (
+          {data.steps.map((step, index) => (
             <li key={index} className="mb-4">
               <p className="font-semibold">{step.step}</p>
               <p>{step.note}</p>
@@ -28,8 +24,8 @@ const BehavioralActivitiesCard = ({
       </section>
 
       <section className="mb-6">
-        <p>{activity.conclusion}</p>
-        <p>{activity.afterThoughts}</p>
+        <p>{data.conclusion}</p>
+        <p>{data.afterThoughts}</p>
       </section>
     </div>
   );
