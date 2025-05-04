f4dd8097af855c20540d5d0fc4b76b9dc0628727
Tina Huynh <tinamchuynh@gmail.com>
2025-05-03 21:49:07 -0700

    Updates image sources for galleries with dynamic fallback
    Replaces static image URLs in the MasonryGridGallery and MixedGallery components with dynamic sources from the provided data array.
    
    Each image now falls back to a predefined URL if no corresponding data is available, improving flexibility and user experience.
    
    Relates to user story #456

--- src/components/images/MasonryGridGallery.tsx
+++ src/components/images/MasonryGridGallery.tsx
@@ -7,7 +7,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[16em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            src={
+              data[0] ||
+              "https://img.freepik.com/free-photo/elderly-woman-drinking-coffee-cafe-while-working-her-laptop-writing-her-notebook_23-2149230374.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={1950}
             height={1300}
@@ -16,7 +19,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[40em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+            src={
+              data[1] ||
+              "https://img.freepik.com/free-photo/close-up-hand-writing-notebook_23-2148877761.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={927}
             height={618}
@@ -25,7 +31,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[15em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+            src={
+              data[2] ||
+              "https://img.freepik.com/free-photo/caucasian-woman-writing-letter_53876-47106.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={2940}
             height={1960}
@@ -36,7 +45,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[33em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            src={
+              data[3] ||
+              "https://img.freepik.com/premium-photo/coffee-shop-cafe-drinking-friendship-togetherness-concept_53876-54879.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={687}
             height={458}
@@ -45,7 +57,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[19em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+            src={
+              data[4] ||
+              "https://img.freepik.com/premium-photo/cropped-hand-woman-writing-with-pen-paper-home_1048944-25455010.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={800}
             height={533}
@@ -54,7 +69,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[19em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+            src={
+              data[5] ||
+              "https://img.freepik.com/free-photo/unrecognisable-man-writing-notebook_23-2148422490.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={500}
             height={500}
@@ -65,7 +83,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[22em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+            src={
+              data[6] ||
+              "https://img.freepik.com/free-photo/unrecognizable-businesswoman-working-office-writing-business-plans-her-notebook_637285-89.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={2940}
             height={1960}
@@ -74,7 +95,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[16em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+            src={
+              data[7] ||
+              "https://img.freepik.com/premium-photo/young-woman-works-laptop-takes-notes-notebook_182615-426.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={500}
             height={500}
@@ -83,7 +107,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[33em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            src={
+              data[8] ||
+              "https://img.freepik.com/free-photo/vertical-selective-closeup-female-writing-notebook-with-blue-pen_181624-3170.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={687}
             height={458}
@@ -94,7 +121,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[31em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            src={
+              data[9] ||
+              "https://img.freepik.com/premium-photo/flatlay-women-s-hands-writing-notebook-sheet-comfortable-home-office-workspace-interior-design_408798-8573.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={687}
             height={458}
@@ -103,7 +133,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[30em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+            src={
+              data[10] ||
+              "https://img.freepik.com/free-photo/top-view-adult-correcting-grammar-mistakes_23-2150171388.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={927}
             height={618}
@@ -112,7 +145,10 @@ export function MasonryGridGallery({ data }: { data: string[] }) {
         <div>
           <Image
             className="rounded-lg max-w-full h-[10em] object-cover object-center"
-            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+            src={
+              data[11] ||
+              "https://img.freepik.com/premium-photo/midsection-woman-working-desk_1048944-11352480.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+            }
             alt="gallery-photo"
             width={927}
             height={500}
--- src/components/images/MixedGallery.tsx
+++ src/components/images/MixedGallery.tsx
@@ -11,7 +11,7 @@ export default function MixedGallery({ data }: { data: string[] }) {
               className="block rounded-lg w-full h-[20em] object-cover object-center"
               src={
                 data[0] ||
-                "https://img.freepik.com/premium-photo/concept-using-data-analysis-systems-with-artificial-intelligence_150418-2110.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+                "https://img.freepik.com/free-photo/working-office_1150-108.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
               }
               width={500}
               height={500}
@@ -23,7 +23,7 @@ export default function MixedGallery({ data }: { data: string[] }) {
               className="block rounded-lg w-full h-[20em] object-cover object-center"
               src={
                 data[1] ||
-                "https://img.freepik.com/premium-photo/martech-marketing-technology-concept-virtual-screen-interface-network-connection-online-market_1101054-70301.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+                "https://img.freepik.com/premium-photo/cropped-hand-woman-writing-book_1048944-30546488.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
               }
               width={500}
               height={500}
@@ -35,7 +35,7 @@ export default function MixedGallery({ data }: { data: string[] }) {
               className="block rounded-lg w-full h-[42em] object-cover object-center"
               src={
                 data[2] ||
-                "https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010144.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+                "https://img.freepik.com/premium-photo/freelancer-yellow-shirt-making-notes-her-notebook_146259-248.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
               }
               width={1000}
               height={500}
@@ -49,7 +49,7 @@ export default function MixedGallery({ data }: { data: string[] }) {
               className="block rounded-lg w-full h-[30em] object-cover object-center"
               src={
                 data[3] ||
-                "https://img.freepik.com/premium-photo/screen-with-word-blue-it_1239820-2062.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+                "https://img.freepik.com/free-photo/student-female-hands-performing-written-task-copybook_1163-2552.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
               }
               width={1000}
               height={500}
@@ -61,7 +61,7 @@ export default function MixedGallery({ data }: { data: string[] }) {
               className="block rounded-lg w-full h-[32em] object-cover object-center"
               src={
                 data[4] ||
-                "https://img.freepik.com/premium-photo/big-data-technology-business-finance-concept_31965-3079.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+                "https://img.freepik.com/free-photo/elderly-woman-drinking-coffee-cafe-while-working-her-laptop-writing-her-notebook_23-2149230374.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
               }
               width={500}
               height={500}
@@ -73,7 +73,7 @@ export default function MixedGallery({ data }: { data: string[] }) {
               className="block rounded-lg w-full h-[32em] object-cover object-center"
               src={
                 data[5] ||
-                "https://img.freepik.com/premium-photo/two-asian-businessmen-analysts-modern-office-reviewing-financial-statements-business-performance-analysing-return-investment-roi_101984-115.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+                "https://img.freepik.com/free-photo/medium-shot-woman-writing-her-journal_23-2150416389.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
               }
               width={500}
               height={500}
