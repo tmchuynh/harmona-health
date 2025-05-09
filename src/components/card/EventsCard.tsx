1fc5c70d84753e90af1f7c6e26951339faa5ba3b
Tina Huynh <tinamchuynh@gmail.com>
2025-05-08 20:52:56 -0700

    Refactors EventsCard layout for improved structure and consistency

--- src/components/card/EventsCard.tsx
+++ src/components/card/EventsCard.tsx
@@ -6,12 +6,12 @@ export default function EventsCard({ event }: { event: Events }) {
       key={event.id}
       className="group flex flex-col items-start max-w-xl"
     >
-      <div className="flex items-center gap-x-4 text-xs">
+      <div className="flex justify-between items-center gap-x-4 w-full text-xs">
+        <p className="uppercase">{event.category}</p>
         <time dateTime={event.date}>{event.date}</time>
-        <a href={event.href}>{event.category}</a>
       </div>
       <div className="relative">
-        <h4 className="underline-offset-4 group-hover:underline no-underline decoration-1">
+        <h4 className="mt-2 underline-offset-4 group-hover:underline no-underline decoration-1">
           <a href={event.href}>{event.title}</a>
         </h4>
         <p>{event.description}</p>
