2b9f6df42b212474e052978ba6b3f0e1f7eb5aab
Tina Huynh <tinamchuynh@gmail.com>
2025-05-09 23:46:15 -0700

    Refactor tool data handling and enhance UI with card components

--- src/app/wellness-library/digital-resources/mental-health-toolkit/(group)/strong-communication-practices/[tools]/page.tsx
+++ src/app/wellness-library/digital-resources/mental-health-toolkit/(group)/strong-communication-practices/[tools]/page.tsx
@@ -1,13 +1,15 @@
 "use client";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { useToolContext } from "@/context/toolContext";
-import { Activity } from "@/lib/interfaces&types/resources";
-import { getToolResource } from "@/lib/utils";
+import { CommunicationTools } from "@/lib/interfaces&types/resources";
+import { cn, getToolResource } from "@/lib/utils";
 import { formatUrlToID } from "@/lib/utils/format";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 export default function Page() {
   const { tool, toolKit, toolKitID, toolInformation } = useToolContext();
   const [loading, setLoading] = useState(true);
-  const [toolData, setToolData] = useState<Activity[]>([]);
+  const [toolData, setToolData] = useState<CommunicationTools[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,8 +27,6 @@ export default function Page() {
     fetchData();
   }, [toolKit, toolKitID]);
 
-  console.log("toolData", toolData);
-
   return (
     <div className="mx-auto">
       {toolInformation?.subtitle && <h2>{toolInformation.subtitle}</h2>}
@@ -35,6 +35,47 @@ export default function Page() {
         toolInformation.introduction.map((intro: string, index: number) => (
           <p key={index}>{intro}</p>
         ))}
+
+      <section className="gap-4 xl:gap-8 grid lg:grid-cols-2 my-8">
+        {toolData.map((data, index) => (
+          <Card key={index}>
+            <CardHeader>
+              <h3>{data.title}</h3>
+              <p>{data.description}</p>
+            </CardHeader>
+            <CardContent>
+              <section className="gap-5 grid lg:grid-cols-5 my-3">
+                {data.image && (
+                  <Image
+                    src={data.image}
+                    alt={data.title}
+                    className="col-span-2 rounded-lg w-full h-full object-cover object-center aspect-square"
+                    width={500}
+                    height={300}
+                  />
+                )}
+                <div
+                  className={cn("col-span-3 flex flex-col gap-3 h-full", {
+                    "col-span-5": !data.image,
+                  })}
+                >
+                  {data.items.item.map((item, iIndex) => (
+                    <div key={iIndex}>
+                      {item.title && <h4>{item.title}</h4>}
+                      {item.description && <p>{item.description}</p>}
+                      <ul className="list-inside list=[upper-roman]">
+                        {item.examples.map((example, eIndex) => (
+                          <li key={eIndex}>{example}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+                </div>
+              </section>
+            </CardContent>
+          </Card>
+        ))}
+      </section>
     </div>
   );
 }
