8bf0a5ac832d4e2a18f08050aa863d9f67b61ecd
Tina Huynh <tinamchuynh@gmail.com>
2025-05-10 00:08:23 -0700

    Refactors JournalPromptCard to use Card component
    Replaces div with Card component for better UI structure
    Updates list style to use upper-roman for improved readability
    
    Enhances code maintainability and visual consistency

--- src/components/card/JournalPromptCard.tsx
+++ src/components/card/JournalPromptCard.tsx
@@ -1,5 +1,6 @@
 import { JournalPrompts } from "@/lib/interfaces&types/resources";
 import { FaQuestionCircle } from "react-icons/fa";
+import { Card } from "../ui/card";
 import {
   Dialog,
   DialogContent,
@@ -14,9 +15,9 @@ export default function JournalPromptCard({
   randomPrompt: JournalPrompts;
 }) {
   return (
-    <div
+    <Card
       key={randomPrompt.prompt}
-      className="relative flex flex-col justify-center items-center gap-6 bg-card shadow-md mx-auto p-4 rounded-lg md:w-2/3 lg:w-1/2 min-h-[15rem]"
+      className="relative flex flex-col justify-center items-center gap-6 mx-auto p-4 md:w-2/3 lg:w-1/2 min-h-[15rem]"
     >
       <randomPrompt.Icon className="size-8" />
 
@@ -29,13 +30,13 @@ export default function JournalPromptCard({
           <DialogHeader className="pt-4">
             <DialogTitle>Ideas To Get Started:</DialogTitle>
           </DialogHeader>
-          <ul className="pl-5 list-disc">
+          <ul className="pl-5 list-[upper-roman] list-outside">
             {randomPrompt.ideas?.map((idea, index) => (
               <li key={index}>{idea}</li>
             ))}
           </ul>
         </DialogContent>
       </Dialog>
-    </div>
+    </Card>
   );
 }
